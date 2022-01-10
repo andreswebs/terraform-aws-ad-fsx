c68e6491890553f48f60db77f5348bf7823de4bb
Andre Silva <andreswebs@pm.me>
2022-01-10 12:39:12 -0300

    fix key alias

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ resource "aws_kms_key" "this" {
 }
 
 resource "aws_kms_alias" "this" {
-  name          = var.kms_key_name
+  name          = "alias/${var.kms_key_name}"
   target_key_id = aws_kms_key.this.key_id
 }
 
