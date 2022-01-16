76824836c3691168f64af2471b64fbc97f4a0b61
Andre Silva <andreswebs@pm.me>
2022-01-16 12:34:58 -0300

    update var validation

--- variables.tf
+++ variables.tf
@@ -46,9 +46,9 @@ variable "fsx_file_system_name" {
 variable "fsx_deployment_type" {
   type = string
   description = "FSx deployment type"
-  default = "MULTI_AZ_1"
+  default = "SINGLE_AZ_2"
   validation {
-    condition     = can(regex("^SINGLE_AZ_1|SINGLE_AZ_2|MULTI_AZ_1$", var.loki_mode))
+    condition     = can(regex("^SINGLE_AZ_1|SINGLE_AZ_2|MULTI_AZ_1$", var.fsx_deployment_type))
     error_message = "Must be one of `SINGLE_AZ_1` or `SINGLE_AZ_2` or `MULTI_AZ_1`."
   }
 }
@@ -58,7 +58,7 @@ variable "fsx_storage_type" {
   description = "FSx storage type"
   default = "SSD"
    validation {
-    condition     = can(regex("^SSD|HDD$", var.loki_mode))
+    condition     = can(regex("^SSD|HDD$", var.fsx_storage_type))
     error_message = "Must be one of `SSD` or `HDD`."
   } 
 }
